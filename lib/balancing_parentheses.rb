e596680736454fc619217f65cb48365d686a81ff
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-21 14:36:44 -0400

    commit tree 35dbbc3979870d805ea3c4b5c764eb6c4506771a

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,39 @@
 require_relative './stack'
+require 'pry'
 
-# your code here
+def balancing_parentheses(input)
+    stack = Stack.new
+    input.chars do |value|
+        stack.push(value)
+    end
+
+    left = 0
+    right = 0
+    extra = 0
+    size = stack.size
+
+    for x in 0..stack.size
+        pop = stack.pop
+        peek = stack.peek
+        if pop == "(" && peek == ")" && x == 0
+            extra+=1
+        elsif pop == "("
+            left+=1
+        elsif pop == ")"
+            right+=1
+        end
+    end
+
+    puts right
+    puts left
+    puts size
+
+    if right > left
+        return right - left + extra
+    elsif left > right
+        return left - right + extra 
+    elsif right == left 
+        return extra
+    end
+end
+
